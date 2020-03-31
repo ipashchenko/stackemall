6e2a10f25e851c82f8c9058cb52c730ab89eb3b0
Ilya Pashchenko <in4pashchenko@gmail.com>
2020-03-31 15:39:14 +0300

    Problems with starting it from bash.

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then
-  echo "Specify source as single positional parameter!"
-  exit 1
-fi
-
-bash -c "exec -a 'MOJAVE polarization stacking --- ' python run_mojave_source.py $1"
